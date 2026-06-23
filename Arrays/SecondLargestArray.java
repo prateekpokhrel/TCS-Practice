4610c5af309aa14b9c45fbb68e5db913ef8774fe
prateekpokhrel <prateekpokhrel000@gmail.com>
2026-06-23 08:01:47 +0545

    commit all

---
+++ Arrays/SecondLargestArray.java
@@ -0,0 +1,24 @@
+// package Arrays;
+
+// public class SecondLargestArray {
+    //  int n = arr.length;
+    //     if (n < 2)
+    //     return -1;
+        
+    //     int first = Integer.MIN_VALUE;
+    //     int second = Integer.MIN_VALUE;
+        
+    //     for (int num : arr){
+    //         if(num > first){
+    //             second = first;
+    //             first = num;
+    //         } else if (num > second && num != first){
+    //             second = num;
+    //         }
+    //     }
+    //     if (second == Integer.MIN_VALUE)
+    //     return -1;
+    //     else 
+    //     return second;
+    // }
+// }
